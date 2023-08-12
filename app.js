2f01e180b1b1db3ba94818ffd50298e6a0c1438e
razzle-code <125223820+razzle-code@users.noreply.github.com>
2023-08-12 13:51:34 +0200

    added game board

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ let web3;
     }
 })();
 
-
 const contractAddress = "0x37726917e0e8D60c4262eFbd79220F52203FcD39";
 const connect4Contract = new web3.eth.Contract([{"inputs":[],"name":"createGame","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"games","outputs":[{"internalType":"address","name":"player1","type":"address"},{"internalType":"address","name":"player2","type":"address"},{"internalType":"uint256","name":"betAmount","type":"uint256"},{"internalType":"bool","name":"isActive","type":"bool"},{"internalType":"enum Connect4.Player","name":"currentPlayer","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"gamesCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"gameId","type":"uint256"}],"name":"joinGame","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint256","name":"gameId","type":"uint256"},{"internalType":"uint8","name":"column","type":"uint8"}],"name":"makeMove","outputs":[],"stateMutability":"nonpayable","type":"function"}], contractAddress);
 
@@ -74,6 +73,40 @@ async function joinGame(gameId) {
         // Here you can also navigate to the game board to start playing
     } else {
         alert("This game is already full!");
+    }
+}
+
+async function makeMove(column) {
+    // Check if the game is valid and if it's the player's turn
+    // For simplicity, we're assuming the player is always PLAYER1 in this example
+    const game = await connect4Contract.methods.games(currentGameId).call();
+    if (game.isActive && game.currentPlayer === "1") {
+        try {
+            await connect4Contract.methods.makeMove(currentGameId, column).send({ from: accounts[0] });
+            // Update the board visually
+            updateBoard();
+        } catch (error) {
+            console.error("Error making a move:", error);
+        }
+    } else {
+        alert("It's not your turn!");
+    }
+}
+
+async function updateBoard() {
+    const game = await connect4Contract.methods.games(currentGameId).call();
+    for (let row = 0; row < 6; row++) {
+        for (let col = 0; col < 7; col++) {
+            const cell = document.querySelector(`#row${row} td:nth-child(${col + 1})`);
+            const value = game.board[row][col];
+            if (value === "1") {
+                cell.textContent = "X"; // Player 1
+            } else if (value === "2") {
+                cell.textContent = "O"; // Player 2
+            } else {
+                cell.textContent = "";
+            }
+        }
     }
 }
 
